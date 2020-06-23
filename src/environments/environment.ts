452eba64088e69e111bb0df18249fb2e2877c29e
DavidNguyen99 <58200614+DavidNguyen99@users.noreply.github.com>
2020-06-23 17:03:37 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,9 @@
-import { SnotifyPosition } from 'ng-snotify';
-
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl : `${window.location.protocol}//${window.location.hostname}/portfolio/`,
-
-  snotifyConfig : {
-    showProgressBar: false,
-    position: SnotifyPosition.rightTop,
-  },
+  production: false
 };
 
 /*
