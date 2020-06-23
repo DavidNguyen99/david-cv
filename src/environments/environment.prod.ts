6286a2bbc326d45b844ea1a6d2300c2362d97615
DavidNguyen99 <58200614+DavidNguyen99@users.noreply.github.com>
2020-06-23 17:02:52 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
-import { SnotifyPosition } from 'ng-snotify';
-
 export const environment = {
-  production: true,
-  baseUrl : `${window.location.protocol}//${window.location.hostname}/portfolio/`,
-
-  snotifyConfig : {
-    showProgressBar: false,
-    position: SnotifyPosition.rightTop,
-  },
+  production: true
 };
